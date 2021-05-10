9128248d2aceab14020f775fbdd5a1c37bd840a9
carlos-seda-gambin <carlossedagambin@gmail.com>
2021-05-10 23:15:17 +0200

    Locale

--- app/Http/Controllers/Front/FaqController.php
+++ app/Http/Controllers/Front/FaqController.php
@@ -5,22 +5,55 @@ namespace App\Http\Controllers\Front;
 use Illuminate\Support\Facades\View;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use Jenssegers\Agent\Agent;
+use App\Vendor\Locale\Locale;
 use App\Models\DB\Faq;
+use App;
+use Debugbar;
 
 class FaqController extends Controller
 {
+    protected $agent;
     protected $faq;
+    protected $locale;
 
-    function __construct(Faq $faq)
+    function __construct(Faq $faq, Locale $locale, Agent $agent)
     {
+        $this->agent = $agent;
         $this->faq = $faq;
+        $this->locale = $locale;
+        
+        $this->locale->setParent('faqs');
+        $this->locale->setLanguage(App::getLocale());
     }
 
     public function index()
-    {
+    {        
+        
+        // $locale = $this->locale->getAllByLanguage();
+        
+        // $faqs = $this->faq->with(['locale' => function($query){
+        //     $query->pluck('value','tag')->toArray();
+        // }])->where('active', 1)->get();
+
+        // $faqs = $faqs->map(function($faq) {  
+        //     return collect($faq)->union($faq->locale->pluck('value','tag'));
+        // });
+
+        $faqs = $this->faq->with('image_featured_desktop')->where('active', 1)->where('visible', 1)->get();
+
+        $faqs = $faqs->each(function($faq) {  
+            
+            $faq['locale'] = $faq->locale->pluck('value','tag');
+            
+            return $faq;
+        });
+
+        Debugbar::info($faqs);
+
 
         $view = View::make('front.pages.faqs.index')
-                ->with('faqs', $this->faq->where('active', 1)->get());
+                ->with('faqs', $faqs );
 
         return $view;
     }
