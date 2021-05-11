5ef0c945f542cdb788ca686c2243a32fa39cc101
carlos-seda-gambin <carlossedagambin@gmail.com>
2021-05-11 13:21:21 +0200

    Locale

--- resources/views/front/pages/faqs/desktop/faqs.blade.php
+++ resources/views/front/pages/faqs/desktop/faqs.blade.php
@@ -1,19 +1,30 @@
 <div class="faqs">
 
+    <div class="faqs-title">
+        <h3>@lang('front/faqs.title')</h3>
+    </div>
+    
     @foreach ($faqs as $faq)
         <div class="faq" data-content="{{$loop->iteration}}">
             <div class="faq-title-container">
-
                 <div class="faq-title">
-                    <h3>{{$faq->title}}</h3>
+                    <h3>{{isset($faq->locale['title']) ? $faq->locale['title'] : ""}}</h3>
                 </div>
 
                 <div class="faq-plus-button" data-button="{{$loop->iteration}}"></div>
             </div>
             <div class="faq-description">
-                <p>{{$faq->description}}</p>
+                <div class="faq-description-text">
+                    {!!isset($faq->locale['description']) ? $faq->locale['description'] : "" !!}
+                </div>
+
+                @isset($faq->image_featured_desktop->path)
+                    <div class="faq-description-image">
+                        <img src="{{Storage::url($faq->image_featured_desktop->path)}}" alt="{{$faq->image_featured_desktop->alt}}" title="{{$faq->image_featured_desktop->title}}" />
+                    </div>
+                @endif
             </div>
         </div>
     @endforeach
-
+    
 </div>
--- resources/views/front/pages/faqs/mobile/faqs.blade.php
+++ resources/views/front/pages/faqs/mobile/faqs.blade.php
@@ -1,21 +1,30 @@
 <div class="faqs">
 
+    <div class="faqs-title">
+        <h3>@lang('front/faqs.title')</h3>
+    </div>
+    
     @foreach ($faqs as $faq)
         <div class="faq" data-content="{{$loop->iteration}}">
             <div class="faq-title-container">
-
                 <div class="faq-title">
-                    {{-- <h3>{{$faq->title}}</h3> --}}
-                    <h3>Esto es una prueba de título</h3>
+                    <h3>{{isset($faq->locale['title']) ? $faq->locale['title'] : ""}}</h3>
                 </div>
 
                 <div class="faq-plus-button" data-button="{{$loop->iteration}}"></div>
             </div>
             <div class="faq-description">
-                {{-- <p>{{$faq->description}}</p> --}}
-                <p>Esto es una prueba de descripción.</p>
+                <div class="faq-description-text">
+                    {!!isset($faq->locale['description']) ? $faq->locale['description'] : "" !!}
+                </div>
+
+                @isset($faq->image_featured_desktop->path)
+                    <div class="faq-description-image">
+                        <img src="{{Storage::url($faq->image_featured_mobile->path)}}" alt="{{$faq->image_featured_mobile->alt}}"  title="{{$faq->image_featured_mobile->title}}"/>
+                    </div>
+                @endif
             </div>
         </div>
     @endforeach
-
+    
 </div>
