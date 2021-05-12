178c8a5f2d0bc8b104ea4d86984fa069778ef122
carlos-seda-gambin <carlossedagambin@gmail.com>
2021-05-12 08:59:40 +0200

    component image

---
+++ resources/views/admin/components/upload.blade.php
@@ -0,0 +1,12 @@
+@if($type == "image" )
+    <div class="upload">      
+        @foreach ($files as $image)
+            @if($image->language == $alias)
+                <div class="upload-thumb" data-label="{{$image->filename}}" style="background-image: url({{Storage::url($image->path)}})"></div>
+            @endif
+        @endforeach
+
+        <span class="upload-prompt">@lang('admin/upload.image')</span>
+        <input class="upload-input" type="file" name="images[{{$content}}.{{$alias}}]">
+    </div>
+@endif
